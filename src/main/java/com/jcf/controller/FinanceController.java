352032a2c33bcc5b90c77aaa8286e9bcb6ccd8f1
Oleg-Storozhev <olegStoroshev@gmail.com>
2021-07-18 18:56:58 +0300

    remove templates and changed pom.xml

--- src/main/java/com/jcf/controller/FinanceController.java
+++ src/main/java/com/jcf/controller/FinanceController.java
@@ -1,16 +1,19 @@
 package com.jcf.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class FinanceController {
     @GetMapping
-    public String main(@RequestParam(name = "name", required = false, defaultValue = "user") String name, Model model) {
+    public String main(
+            @RequestParam(name = "name", required = false, defaultValue = "user") String name,
+            Model model
+    ){
         model.addAttribute("name", name);
-        return "main";
+        return "Hello, " + name;
     }
 }
