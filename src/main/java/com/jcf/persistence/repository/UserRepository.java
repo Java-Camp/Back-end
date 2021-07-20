9b8a25e960d77a964508be43092208e9a8456420
Oleg-Storozhev <olegStoroshev@gmail.com>
2021-07-20 22:33:16 +0300

    added save method in UserRepository used jdbcTemplate

--- src/main/java/com/jcf/persistence/repository/UserRepository.java
+++ src/main/java/com/jcf/persistence/repository/UserRepository.java
@@ -1,34 +1,42 @@
 package com.jcf.persistence.repository;
 
 import com.jcf.persistence.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.Objects;
 import java.util.Optional;
 
 @Repository
 public class UserRepository implements CrudRepository<User, Long> {
 
-    @PersistenceContext
-    EntityManager entityManager;
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public User save(User entity) {
         Assert.notNull(entity, "Entity must be a set");
+        String Query;
         if (Objects.isNull(entity.getId())) {
-            entityManager.persist(entity);
-        } else {
-            return entityManager.merge(entity);
+            Query = "INSERT INTO \"user\" (first_name, last_name, email, password, id) VALUES (?, ?, ?, ?, ?)";
         }
+        else{
+            Query = "UPDATE \"user\" SET first_name = ?, last_name = ?, email = ?, password = ?  where id = ?";
+        }
+        jdbcTemplate.update(Query, entity.getFirstName(), entity.getLastName(), entity.getEmail(), entity.getPassword(), entity.getId());
         return entity;
     }
 
     @Override
     public Optional<User> findById(Long id) {
         if (Objects.isNull(id)) return Optional.empty();
-        return Optional.ofNullable(entityManager.find(User.class, id));
+        // todo: logic
+        return Optional.empty();
     }
 }
