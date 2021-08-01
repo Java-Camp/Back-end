e0d05cfb5a00ecf96277647ca09656ec45515dae
Oleg-Storozhev <olegStoroshev@gmail.com>
2021-08-01 19:25:39 +0300

    added Aspect

---
+++ src/main/java/com/jcf/aspects/CheckAspect.java
@@ -0,0 +1,44 @@
+package com.jcf.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+@Slf4j
+@Aspect
+public class CheckAspect {
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+
+    private TransactionStatus status = null;
+
+    @Pointcut("execution(* com.jcf.service.*.*(..))") // expression
+    private void businessService() {}  // signature
+
+    @Before("businessService()")
+    public void doBeforeTask(){
+        log.info("Transaction was started");
+        DefaultTransactionDefinition paramTransactionDefinition = new DefaultTransactionDefinition();
+        status = platformTransactionManager.getTransaction(paramTransactionDefinition);
+    }
+
+    @After("businessService()")
+    public void doAfterTask(){
+        status = null;
+    }
+
+    @AfterReturning("businessService()")
+    public void doAfterReturnningTask(){
+        log.info("Transaction was done successful.");
+        platformTransactionManager.commit(status);
+    }
+
+    @AfterThrowing(value = "businessService()", throwing = "ex")
+    public void doAfterThrowingTask(Exception ex){
+        log.info("Transaction was canceled. Exception was thrown:\n" + ex);
+        platformTransactionManager.rollback(status);
+    }
+}
--- src/main/java/com/jcf/service/UserServiceImpl.java
+++ src/main/java/com/jcf/service/UserServiceImpl.java
@@ -4,17 +4,13 @@ import com.jcf.persistence.repository.UserRepository;
 import com.jcf.vo.UserVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.*;
 
@@ -27,16 +23,12 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepo;
     private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private PlatformTransactionManager platformTransactionManager;
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepo.findByEmail(username);
-        if (user == null) {
-            log.error("User not found");
+        if (user == null)
             throw new UsernameNotFoundException("User not found");
-        } else log.error("User found: {}", username);
+         else log.error("User found: {}", username);
 
         Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority(user.getRole()));
@@ -47,33 +39,25 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     public User saveUser(UserVO vo) {
         log.info("Saving new user to database");
         vo.setPassword(passwordEncoder.encode(vo.getPassword()));
-        DefaultTransactionDefinition paramTransactionDefinition = new DefaultTransactionDefinition();
-        TransactionStatus status = platformTransactionManager.getTransaction(paramTransactionDefinition);
-        try {
-            User user = new User();
-            if (Objects.isNull(vo.getFirstName()))
-                throw new IllegalArgumentException("First name is null");
-            if (Objects.isNull(vo.getLastName()))
-                throw new IllegalArgumentException("Last name is null");
-            if (Objects.isNull(vo.getEmail()))
-                throw new IllegalArgumentException("Email is null");
-            if (Objects.isNull(vo.getPassword()))
-                throw new IllegalArgumentException("Password is null");
+        User user = new User();
+        if (Objects.isNull(vo.getFirstName()))
+            throw new IllegalArgumentException("First name is null");
+        if (Objects.isNull(vo.getLastName()))
+            throw new IllegalArgumentException("Last name is null");
+        if (Objects.isNull(vo.getEmail()))
+            throw new IllegalArgumentException("Email is null");
+        if (Objects.isNull(vo.getPassword()))
+            throw new IllegalArgumentException("Password is null");
 
-            user.setFirstName(vo.getFirstName());
-            user.setLastName(vo.getLastName());
-            user.setEmail(vo.getEmail());
-            user.setPassword(vo.getPassword());
-            user.setRole("USER");
-            User user1 = userRepo.saveOrUpdate(user);
-            user1.setId(getUserByEmail(user.getEmail()).getId());
-            platformTransactionManager.commit(status);
-            return user1;
-        }
-        catch (Exception ex){
-            platformTransactionManager.rollback(status);
-            throw ex;
-        }
+        user.setFirstName(vo.getFirstName());
+        user.setLastName(vo.getLastName());
+        user.setEmail(vo.getEmail());
+        user.setPassword(vo.getPassword());
+        user.setRole("USER");
+        User user1 = userRepo.saveOrUpdate(user);
+        user1.setId(getUserByEmail(user.getEmail()).getId());
+
+        return user1;
     }
 
     @Override
@@ -98,23 +82,15 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
     @Override
     public void delete(Long id) {
-        DefaultTransactionDefinition paramTransactionDefinition = new DefaultTransactionDefinition();
-        TransactionStatus status = platformTransactionManager.getTransaction(paramTransactionDefinition);
-        try{
-            if (userRepo.findById(id).isEmpty())
-                throw new IllegalArgumentException("No entity with such id");
 
-            userRepo.delete(id);
+        if (userRepo.findById(id).isEmpty())
+            throw new IllegalArgumentException("No entity with such id");
 
-            if (userRepo.findById(id).isPresent())
-                throw new RuntimeException("Delete is not working");
+        userRepo.delete(id);
 
-            platformTransactionManager.commit(status);
-        }
-        catch (Exception ex){
-            platformTransactionManager.rollback(status);
-            throw ex;
-        }
+        if (userRepo.findById(id).isPresent())
+            throw new RuntimeException("Delete is not working");
+
     }
 
 }
