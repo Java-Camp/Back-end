2a74f2c32effd98fc28b1ec11eceae3ae2f9e320
Vova <volodymyr.vernyba@gmail.com>
2021-08-03 20:23:09 +0300

    Fix method findAll. Add find by id method to controller

--- src/main/java/com/jcf/api/CurrencyController.java
+++ src/main/java/com/jcf/api/CurrencyController.java
@@ -6,6 +6,7 @@ import com.jcf.service.CurrencyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +22,10 @@ public class CurrencyController {
     public ResponseEntity<List<Currency>> getCurrencies() {
         return ResponseEntity.ok().body(currencyService.getAllCurrency());
     }
+
+    @GetMapping("/currencies/{id}")
+    public Currency getCurrencyById(@PathVariable Long id) {
+        return currencyService.getById(id);
+    }
+
 }
--- src/main/java/com/jcf/service/CurrencyService.java
+++ src/main/java/com/jcf/service/CurrencyService.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface CurrencyService {
     List<Currency> getAllCurrency();
+    Currency getById(Long id);
 }
