c0756768fe92b6d36d5babbc15462df4f4962274
Oleg-Storozhev <olegStoroshev@gmail.com>
2021-08-11 20:29:36 +0300

    added update @PUTController

--- src/main/java/com/jcf/service/OperationServiceImpl.java
+++ src/main/java/com/jcf/service/OperationServiceImpl.java
@@ -67,7 +67,7 @@ public class OperationServiceImpl implements OperationService{
         log.info("user id " + user.getId());
         log.info("user id " + user.getId());
 
-        if(!(user.getId() == operation.getAccountId().longValue()))
+        if(!(user.getId().equals(operation.getAccountId().longValue())))
             throw new LockedAccessException("You can't do anything with user " + userRepository.findById(operationDTO.getAccountId().longValue()).get().getEmail());
         if (Objects.isNull(operationDTO.getSum()))
             throw new FieldIsNullException("Sum");
